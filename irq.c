6621979a43198faae6a6355120429a93b5ebe3df
DocDilbert <ch.groeling@gmail.com>
2015-08-18 10:06:29 +0200

    Added file header to irq.c

--- irq.c
+++ irq.c
@@ -1,3 +1,9 @@
+/// \file irq.c
+///
+/// File all interrupt serivce handlers.
+///
+/// \author Christian Groeling <ch.groeling@gmail.com>
+
 #include "hw_gpio.h"
 
 /// \brief System tick interrupt service handler
