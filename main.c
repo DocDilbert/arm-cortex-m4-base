2c3ad8029c4cbf81cfa2a2c90cf1ecd44045db81
DocDilbert <ch.groeling@gmail.com>
2015-08-18 09:46:26 +0200

    Added doxygen comments to main.c

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
+/// \file main.c
+///
+/// File containing the main() function which is the starting point of the program.
+///
+/// \author Christian Groeling <ch.groeling@gmail.com>
+
 #include "hw_gpio.h"
 
+
+/// \brief Short explanation of what happens when the system starts.
+///
+/// When the system starts a reset irq is raised. The reset interrupt service
+/// routine currently calls two functions.
+/// * init() - Basic system initalization.
+/// * main() - Starting point.
+///
+/// \defgroup StartSequence System startup sequence
+
+ 
 volatile int test = 0xDEADBEAF;
 volatile test2 = 0x2;
 
@@ -11,6 +28,11 @@ void wait()
         asm("nop");
 }
 
+/// \brief This function is the starting point of the program. 
+///
+/// The function is called after the basic initialization of the system
+/// has finished. It should not be leaved.
+/// \ingroup StartSequence
 void main()
 {
     hw_gpio_init() ;
@@ -30,23 +52,48 @@ void main()
     } 
 }
 
-void _exit(int a)
+/// \brief Hook which is called when exit() is called. 
+/// \retval arg [in] The argument which the user supplied to the exit call.
+void _exit(int arg)
 {
     while(1)
     {
     }
 }
 
+/// \brief System tick interrupt service handler
 void systick_handler()
 {
     HW_GPIO_TOGGLE(DEBUGPIN_1);
 }
 
-extern unsigned long _etext;
-extern unsigned long _data;
-extern unsigned long _edata;
-extern unsigned long _bss;
-extern unsigned long _ebss;
+/// End address of the text section.  
+/// The text section is followed by the initial values of the data section.
+/// This symbol is set by the linker. 
+extern unsigned long _etext; 
+
+/// Start address of the data section. This symbol is set by the linker.
+extern unsigned long _data;  
+
+/// End address of the data section. This symbol is set by the linker.
+extern unsigned long _edata; 
+
+/// Start address of the bss section. This symbol is set by the linker.
+extern unsigned long _bss;   
+
+/// End address of the bss section. This symbol is set by the linker.
+extern unsigned long _ebss;  
+
+/// \brief This function performs the basic system initialization.
+///
+/// When a reset is raised (e.g. at startup) this is the first function which gets called.
+/// It performs the following tasks:
+/// * Copy initial values to the data section in ram.
+/// * Initialize the bss ram section with 0.
+/// * PLL Configuration 
+/// * SysTick Configuration
+///
+/// \ingroup StartSequence
 void init()
 {
     unsigned long *src, *dest;
@@ -65,5 +112,8 @@ void init()
     SystemInit();
     SystemCoreClockUpdate();
     
+	// Set the systick to 1 ms
     SysTick_Config(SystemCoreClock/1000);
 }
+
+/// \}
