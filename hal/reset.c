73482f72d2f29eeeb8a7c8f2f18e96b1ed2cdd0b
DocDilbert <ch.groeling@gmail.com>
2015-08-25 06:57:33 +0200

    The initialization of the bss and data section should now be clearer.

--- hal/reset.c
+++ hal/reset.c
@@ -10,19 +10,19 @@
 /// End address of the text section.  
 /// The text section is followed by the initial values of the data section.
 /// This symbol is set by the linker. 
-extern uint32_t _etext;
+extern uint32_t text_end;
 
 /// Start address of the data section. This symbol is set by the linker.
-extern uint32_t _data;
+extern uint32_t data_start;
 
 /// End address of the data section. This symbol is set by the linker.
-extern uint32_t _edata;
+extern uint32_t data_end;
 
 /// Start address of the bss section. This symbol is set by the linker.
-extern uint32_t _bss;
+extern uint32_t bss_start;
 
 /// End address of the bss section. This symbol is set by the linker.
-extern uint32_t _ebss;
+extern uint32_t bss_end;
 
 /// \brief This function handles the reset irq
 ///
@@ -38,16 +38,18 @@ void isr_reset()
 {
     uint32_t *src, *dest;
 
-    // Initialize the data section in ram with the default values stored in flash.
-    // The default values a stored "after" the text section.
-    src = &_etext;
-    for (dest = &_data; dest < &_edata;)
+    // Initialize the data section in ram with its initial values stored in flash.
+    // The initial values a stored "after" the text section.
+    src = &text_end;
+    dest = &data_start;
+    while (dest < &data_end)
     {
         *dest++ = *src++;
     }
 
     // Initialize the bss section with 0
-    for (src = &_bss; src < &_ebss;)
+    src = &bss_start;
+    while (src < &bss_end)
     {
         *src++ = 0;
     }
