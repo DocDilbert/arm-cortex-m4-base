297ae083bacabee5fac08d076c4a40e7b890b582
DocDilbert <ch.groeling@gmail.com>
2015-08-26 09:11:16 +0200

    syscalls.c: Changed arguments of the _exit function to c standard types

--- syscalls.c
+++ syscalls.c
@@ -3,13 +3,13 @@
 /// File which contains all necessary stdlib system calls.
 ///
 /// \author Christian Groeling <ch.groeling@gmail.com>
-#include <stdint.h>
 
 /// \brief System call which called by the exit() function. 
 /// \retval arg [in] The argument which the user supplied to the exit() call.
-void _exit(int32_t arg)
+void _exit(int arg)
 {
     while (1)
     {
     }
 }
+
