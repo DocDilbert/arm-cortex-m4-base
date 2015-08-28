f6f37c39518040c62c0c33d713b5c01fa654c008
DocDilbert <ch.groeling@gmail.com>
2015-08-28 09:09:56 +0200

    pre_sections.s - Removed underscores in constant names.

--- pre_sections.s
+++ pre_sections.s
@@ -8,23 +8,20 @@
 .thumb
 
 
-// define stack section
-.equ    Stack_Size, 0x00000400 // 1 KByte
+// Define stack section
+.equ StackSize, 0x00000400 // 1 KByte
 .section ".stack", "wa" // create .stack section is writable(w) and allocatable(a)
-.align  3	// stack must be 64 bit aligned
+.align 3	// stack must be 64 bit aligned
 _stack_mem:
-    .space  Stack_Size
+    .space StackSize
 
 
-// define heap section
-// The malloc implementation of the newlibc uses 4 KByte pagesize.
-// If the page size of malloc must be changed the newlibc must be recompiled.
-// Another option is to provide an own implementation of malloc.
-.equ    Heap_Size,  0x00002000 // 8 kByte
+// Define heap section - used by malloc
+.equ HeapSize,  0x00002000 // 8 kByte
 .section ".heap", "wa" // create .heap section is writable(w) and allocatable(a)
-.align  3 // heap must be 64 bit aligned
+.align 3 // heap must be 64 bit aligned
 _heap_mem:
-    .space  Heap_Size
+    .space HeapSize
 
 
 
