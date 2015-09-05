e4ab3eb59ce7fb2c1010295c101b97dc979ff3b0
DocDilbert <ch.groeling@gmail.com>
2015-09-05 10:41:27 +0200

    Removed test code.

--- src/main.cpp
+++ src/main.cpp
@@ -14,43 +14,13 @@
 #include "gpio.h"
 #include "utils.h"
 #include "gpio.h"
-/// @cond TEST
-class A
-{
-public:
-    A()
-    {
 
-    }
-    A(int arg)
-    {
-        member = arg;
-    }
+GpioController gpioCtrl; ///< The gpio controller object.
 
-    ~A()
-    {
-        member = -1;
-    }
-private:
-    int member;
-};
-
-uint32_t data_test = 0xDEADBEAF;
-uint8_t data_test2 = 0xDE;
-uint16_t data_test3 = 0xDEAD;
-
-uint32_t bss_test = 0;
-uint8_t bss_test2;
-uint16_t bss_test3;
-
-const uint32_t read_only = 0x100;
-
-uint16_t array_test[20];
-uint16_t array_test2[10] = { 50, 60, 10 };
-const uint16_t array_test3[10] = { 50, 60, 10 };
-
-volatile A a_static(50);
-
+GpioPin* debug1; ///< Global reference to debug pin 1 object
+GpioPin* debug2; ///< Global reference to debug pin 2 object
+GpioPin* debug3; ///< Global reference to debug pin 3 object
+GpioPin* debug4; ///< Global reference to debug pin 4 object
 
 RAMFUNC void ramTrampoline(GpioPin* debugPin)
 {
@@ -59,17 +29,6 @@ RAMFUNC void ramTrampoline(GpioPin* debugPin)
     UTILS_simulateLoad(1000000);
     debugPin->setOutLow();
 }
-
-/// @endcond
-
-GpioController gpioCtrl; ///< The gpio controller object.
-
-GpioPin* debug1; ///< reference to debug pin 1
-GpioPin* debug2; ///< reference to debug pin 2
-GpioPin* debug3; ///< reference to debug pin 3
-GpioPin* debug4; ///< reference to debug pin 4
-
-
 
 /// @brief This function is the starting point of the program. 
 ///
@@ -80,10 +39,11 @@ GpioPin* debug4; ///< reference to debug pin 4
 int main()
 {
     static uint32_t cycles;
-    volatile float32_t abs_in = -5.0;
-    volatile float32_t abs_out = 0;
-    A* a, *b;
-    void* malloc_test[7];
+
+    // printf: turn off buffers, so IO occurs immediately.
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
 
     // Initialize gpios.
     debug1 = gpioCtrl.getPin<DEBUG_PIN1>();
@@ -97,62 +57,18 @@ int main()
     debug2->init(GPIO_OUTPUT_LOW);
     debug3->init(GPIO_OUTPUT_LOW);
     debug4->init(GPIO_OUTPUT_LOW);
+
     led_red->init(GPIO_OUTPUT_LOW);
 
-    // turn off buffers, so IO occurs immediately
-    setvbuf(stdin, NULL, _IONBF, 0);
-    setvbuf(stdout, NULL, _IONBF, 0);
-    setvbuf(stderr, NULL, _IONBF, 0);
-
-    memset(array_test, 1, 5);
-
-    abs_out = fabs(abs_in);
-
-    a = new A(10);
-    delete (a);
-
-    b = new A[10];
-    delete[] (b);
-
-    malloc_test[0] = malloc(10);
-    malloc_test[1] = malloc(13);
-    malloc_test[2] = malloc(0x100);
-    malloc_test[3] = malloc(0x500);
-    malloc_test[4] = malloc(0x500);
-    malloc_test[5] = malloc(0x500);
-    malloc_test[6] = malloc(0x500);
-
-    debug1->getIn();
-    debug2->getIn();
     while (1)
     {
         printf("Hello World %i\n", cycles);
         cycles++;
-        free(malloc_test[0]);
-        malloc_test[0] = malloc(100);
-        bss_test++;
-        bss_test2++;
-        bss_test3++;
-        data_test++;
-        data_test2++;
-        data_test3++;
 
         led_red->setOutLow(); // red led on - inverse logic.
-
         ramTrampoline(debug2);
-
         led_red->setOutHigh(); // red led off - inverse logic.
-
-        /*GPIO_PUT(DEBUGPIN_2, 1);
-         UTILS_nopUnroll<500>();
-         UTILS_nopUnroll<500>();
-         GPIO_PUT(DEBUGPIN_2, 0);*/
-
         ramTrampoline(debug3);
-
-        debug4->setOutHigh();
-
-        debug4->setOutLow();
     }
     // never leave this function
     return -1;
