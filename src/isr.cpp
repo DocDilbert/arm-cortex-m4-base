2e8037764ab1212a1f271081e445f7e85d3b153b
DocDilbert <ch.groeling@gmail.com>
2015-09-06 16:33:31 +0200

    Introduced dummy class which gets called when no isr is registered.
    This saves one "if" in the isr.

--- src/isr.cpp
+++ src/isr.cpp
@@ -1,24 +1,30 @@
 #include "base_types.h"
 #include "isr.h"
 
-IInterruptServiceRoutine *pSysTickController = NULL; ///< object which is used by the SYSTICK_trampoline.
+struct InterruptServiceRoutineDummy : public IInterruptServiceRoutine
+{
+    void update()
+    {
+        // do nothing
+    }
+};
 
+InterruptServiceRoutineDummy isrDummy;
+IInterruptServiceRoutine *pSysTickIsr = &isrDummy; ///< object which is used by the SYSTICK_trampoline.
 
 void ISR_registerSysTick(IInterruptServiceRoutine *sysTickController)
 {
-    pSysTickController = sysTickController;
+    pSysTickIsr = sysTickController;
 }
-
 
 /// @brief This function is called when the systick irq is raised.
 ///
 /// This function calls the registered interrupt service routine.
 ///
-/// \attention C Linkage is required for all interrupt service routine trampolines.
+/// \attention C Linkage is required for all interrupt service routines.
 extern "C" void ISR_Systick()
 {
-    // Check if a valid object was registered
-    if (pSysTickController != NULL)
-        pSysTickController->update();
+    // Call registered interrupt service routine
+    pSysTickIsr->update();
 }
 
