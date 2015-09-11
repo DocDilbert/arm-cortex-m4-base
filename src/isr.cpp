1bb5670e05f3c93cbe9dc2f67af736d40b3db977
DocDilbert <ch.groeling@gmail.com>
2015-09-11 07:30:04 +0200

    Updated comments in isr.cpp.

--- src/isr.cpp
+++ src/isr.cpp
@@ -68,52 +68,52 @@ void ISR_registerSysTick(IInterruptServiceRoutine *sysTickController)
 ///
 /// This function calls the registered systick interrupt service routine.
 ///
-/// \attention C Linkage is required for all interrupt service routines.
+/// @attention C Linkage is required for all interrupt service routines.
 extern "C" void ISR_Systick()
 {
     // Call registered interrupt service routine
     pSysTickIsr->isr();
 }
 
-/// Hard fault interrupt service routine
+/// @brief Hard fault interrupt service routine
 ///
 /// A HardFault is an exception that occurs because of an error during exception processing, or because
 /// an exception cannot be managed by any other exception mechanism. HardFaults have a fixed priority of -1,
 /// meaning they have higher priority than any exception with configurable priority.
 ///
-/// \attention C Linkage is required for interrupt service routines.
+/// @attention C Linkage is required for interrupt service routines.
 ///
 extern "C" void ISR_HardFault()
 {
     ERROR_handler();
 }
 
-/// Memory manage fault interrupt service routine
+/// @brief Memory manage fault interrupt service routine
 ///
 /// A MemManage fault is an exception that occurs because of a memory protection related fault. The fixed memory
 /// protection constraints determines this fault, for both instruction and data memory transactions. This fault is always
 /// used to abort instruction accesses to Execute Never (XN) memory regions.
 ///
-/// \attention C Linkage is required for interrupt service routines.
+/// @attention C Linkage is required for interrupt service routines.
 ///
 extern "C" void ISR_MemManageFault()
 {
     ERROR_handler();
 }
 
-/// Bus fault interrupt service routine
+/// @brief Bus fault interrupt service routine
 ///
 /// A BusFault is an exception that occurs because of a memory related fault for an instruction or data memory transaction.
 /// This might be from an error detected on a bus in the memory system.
 ///
-/// \attention C Linkage is required for interrupt service routines.
+/// @attention C Linkage is required for interrupt service routines.
 ///
 extern "C" void ISR_BusFault()
 {
     ERROR_handler();
 }
 
-/// Usage fault interrupt service routine
+/// @brief Usage fault interrupt service routine
 ///
 /// A UsageFault is an exception that occurs because of a fault related to instruction execution. This includes:
 /// * an undefined instruction
@@ -125,7 +125,7 @@ extern "C" void ISR_BusFault()
 /// * an unaligned address on word and halfword memory access
 /// * division by zero.
 ///
-/// \attention C Linkage is required for interrupt service routines.
+/// @attention C Linkage is required for interrupt service routines.
 ///
 extern "C" void ISR_UsageFault()
 {
@@ -144,7 +144,7 @@ extern "C" void ISR_UsageFault()
 /// * PLL Configuration
 /// * SysTick Configuration
 ///
-/// \attention C Linkage is required for interrupt service routines.
+/// @attention C Linkage is required for interrupt service routines.
 ///
 /// @ingroup StartSequence
 extern "C" void ISR_Reset()
